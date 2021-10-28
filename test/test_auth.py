e6a3719b46d09ce3490de3f93e137a615c787f64
Piotr <piotr.ostrowski@aol.com>
2021-10-28 13:43:49 +0200

    add base tests for auth

--- test/test_auth.py
+++ test/test_auth.py
@@ -5,3 +5,27 @@ import json
 from test.setup_client import client
 
 
+sample_user = {
+  'username': 'user3',
+  'password': 'secret',
+  'date_of_birth': 1635353891,
+  'email': 'user3@gmail.com'
+}
+
+credentials = {
+    'username': 'user3',
+    'password': 'secret'
+}
+
+
+def test_create_account():
+    response = client.post('/users/', json=sample_user)
+    assert response.status_code == 200
+
+
+def test_login():
+    response = client.post('/token', data=credentials)
+    response = response.json()
+    assert response['access_token'] is not None
+    assert response['token_type'] == 'bearer'
+
