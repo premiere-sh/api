724ffd143676210d6a4562cbe355367557905e66
Piotr <piotr.ostrowski@aol.com>
2021-10-31 14:57:40 +0100

    ditch the env variables for now and use sha for container tag in aws.yml

--- api/database.py
+++ api/database.py
@@ -4,9 +4,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DB_USERNAME = os.getenv('DB_USERNAME')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_HOST = os.getenv('DB_HOST')
+
+# TODO add env vars to ECS (im lazy)
+# DB_USERNAME = os.getenv('DB_USERNAME')
+# DB_PASSWORD = os.getenv('DB_PASSWORD')
+# DB_HOST = os.getenv('DB_HOST')
+ 
+DB_USERNAME = 'postgres' 
+DB_PASSWORD = 'Borowki123'
+DB_HOST = 'premiere-dev.c6mfxalyhupg.us-east-2.rds.amazonaws.com'
 
 engine = create_engine(
     f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/premiere'
