d9f254b92ecd09d97bba8f9d7b3a53a1afa67b01
Piotr <piotr.ostrowski@aol.com>
2021-11-04 15:41:01 +0100

    add tests for invitation

--- test/test_friends.py
+++ test/test_friends.py
@@ -1,11 +1,32 @@
 import pytest
 
 from test.setup_client import client
+from test.sample_user import sample_user1, sample_user2
 
 
-def test_add_friend():
-    res = client.post('/friends/')
+def test_send_invite():
+    user1_id = (client.post('/users/', json=sample_user1)).json()
+    user2_id = (client.post('/users/', json=sample_user2)).json()
+    user1 = (client.get(f'/users/{user1_id}')).json()
+    user2 = (client.get(f'/users/{user2_id}')).json()
+    response = client.post('/token/', data={
+        'username': sample_user1['username'],
+        'password': sample_user1['password']
+    })
+    token = response.json()['access_token']
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+    res = client.post(f'/users/{user2_id}/friends/invite/', headers=headers)
+    friendship = res.json()
+    assert friendship['inviting_friend'] == user1['username']
+    assert friendship['accepting_friend'] == user2['username']
 
+
+def test_accept_invite():
+    pass
 
 def test_only_authorized_can_send_request():
     """
@@ -22,14 +43,9 @@ def test_only_invited_user_can_accept_request():
 
 
 def test_get_friends():
-    return
-    res = client.get('/friends/')
+    res = client.get('/users/1/friends/')
     assert res.status_code == 200
-    friends = res.json()
-    print(friends)
-    assert 0
 
 
 def test_remove_friend():
     pass
-
