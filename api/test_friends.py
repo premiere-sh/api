d2cdb425a26b6db1e1685f2ff1606a1e6c00661a
elanegan <e.lanegan@lancaster.ac.uk>
2022-07-13 12:11:15 +0100

    Fix tests for inviting friends. - Changed invites system. - Also cleared up tests.

--- api/test_friends.py
+++ api/test_friends.py
@@ -4,33 +4,42 @@ from .setup_client import client
 from .sample_user import get_sample_user
 from .test_auth import get_auth_headers
 
+def test_send_invite_requires_auth():
+    user1_id = (client.post('/users/', json=get_sample_user(1))).json()
+    user2_id = (client.post('/users/', json=get_sample_user(2))).json()
+    response = client.post(f'/users/{user1_id}/friends/invite/{user2_id}/')
+    assert response.status_code == 401
+
 
 def test_send_invite():
-    user1_id = (client.post('/users/', json=get_sample_user(1))).json()
+    user1_id = (client.post('/users/', json=get_sample_user(4))).json()
     global user2_id
-    user2_id = (client.post('/users/', json=get_sample_user(2))).json()
-    user1 = (client.get(f'/users/{user1_id}')).json()
-    user2 = (client.get(f'/users/{user2_id}')).json()
-    headers = get_auth_headers(client=client, sample_user_id=1)
-    slug = f'/users/{user2_id}/friends/invite/'
+    user2_id = (client.post('/users/', json=get_sample_user(5))).json()
+    user1 = (client.get(f'/users/{user1_id}/')).json()
+    user2 = (client.get(f'/users/{user2_id}/')).json()
+    headers = get_auth_headers(client=client, sample_user_id=4)
+    slug = f'/users/{user1_id}/friends/invite/{user2_id}/'
     response = client.post(slug, headers=headers)
     friendship = response.json()
     assert friendship['inviting_friend'] == user1['username']
     assert friendship['accepting_friend'] == user2['username']
 
 
-def test_send_invite_requires_auth():
-    user1_id = (client.post('/users/', json=get_sample_user(1))).json()
-    user1 = (client.get(f'/users/{user1_id}')).json()
-    response = client.post(f'/users/{user1_id}/friends/invite')
-    assert response.status_code == 401
+def test_cannot_send_invite_if_there_is_one_sent():
+    user1_id = (client.post('/users/', json=get_sample_user(6))).json()
+    user2_id = (client.post('/users/', json=get_sample_user(7))).json()
+    headers = get_auth_headers(client=client, sample_user_id=6)
+    slug = f'/users/{user1_id}/friends/invite/{user2_id}/'
+    response = client.post(slug, headers=headers)
+    assert response.status_code == 200
+    resent = client.post(slug, headers=headers)
+    assert resent.status_code == 400
 
 
 def test_user_cannot_send_self_invite():
     user111_id = (client.post('/users/', json=get_sample_user(111))).json()
-    user111 = (client.get(f'/users/{user111_id}')).json()
     headers = get_auth_headers(client=client, sample_user_id=111)
-    slug = f'/users/{user111_id}/friends/invite/'
+    slug = f'/users/{user111_id}/friends/invite/{user111_id}/'
     response = client.post(slug, headers=headers)
     assert response.status_code == 403
 
@@ -38,9 +47,8 @@ def test_user_cannot_send_self_invite():
 def test_send_invite_to_non_existent():
     user1_id = (client.post('/users/', json=get_sample_user(88))).json()
     user2_id = (client.post('/users/', json=get_sample_user(99))).json()
-    user1 = (client.get(f'/users/{user1_id}')).json()
     headers = get_auth_headers(client=client, sample_user_id=88)
-    slug = f'/users/999/friends/invite/'
+    slug = f'/users/{user1_id}/friends/invite/999/'
     response = client.post(slug, headers=headers)
     assert response.status_code == 404
 
@@ -57,7 +65,7 @@ def test_user_can_only_see_their_own_invites():
 
 
 def test_get_invites():
-    headers = get_auth_headers(client=client, sample_user_id=2)
+    headers = get_auth_headers(client=client, sample_user_id=5)
     response = client.get(f'/users/{user2_id}/invites/', headers=headers)
     invites = response.json()
     assert response.status_code == 200
@@ -65,7 +73,7 @@ def test_get_invites():
 
 
 def test_accept_invite_requires_auth():
-    headers = get_auth_headers(client=client, sample_user_id=2)
+    headers = get_auth_headers(client=client, sample_user_id=5)
     response = client.get(f'/users/{user2_id}/invites/', headers=headers)
     [invite] = response.json()
     invite['has_been_accepted'] = True
@@ -78,7 +86,7 @@ def test_accept_invite_requires_auth():
 
 
 def test_accept_invite():
-    headers = get_auth_headers(client=client, sample_user_id=2)
+    headers = get_auth_headers(client=client, sample_user_id=5)
     response = client.get(f'/users/{user2_id}/invites/', headers=headers)
     friends_before = (client.get(f'/users/{user2_id}/friends/')).json()
     [invite] = response.json()
@@ -101,11 +109,10 @@ def test_cannot_accepted_already_accepted_invite():
     # send invite
     user1_id = (client.post('/users/', json=get_sample_user(333))).json()
     user2_id = (client.post('/users/', json=get_sample_user(444))).json()
-    user1 = (client.get(f'/users/{user1_id}')).json()
-    user2 = (client.get(f'/users/{user2_id}')).json()
     headers = get_auth_headers(client=client, sample_user_id=333)
-    slug = f'/users/{user2_id}/friends/invite/'
+    slug = f'/users/{user1_id}/friends/invite/{user2_id}/'
     response = client.post(slug, headers=headers)
+    assert response.status_code == 200
     # accept invite
     headers = get_auth_headers(client=client, sample_user_id=444)
     response = client.get(f'/users/{user2_id}/invites/', headers=headers)
@@ -117,6 +124,7 @@ def test_cannot_accepted_already_accepted_invite():
         json=invite,
         headers=headers
     )
+    assert response.status_code == 200
     # accept invite again
     response = client.put(
         f'/users/{user2_id}/friends/invites/accept/', 
@@ -144,10 +152,10 @@ def test_cannot_unfriend_for_others():
 
 
 def test_unfriend():
-    user1 = get_sample_user(1)
+    user1 = get_sample_user(4)
     friends = (client.get(f'/users/{user2_id}/friends/')).json()
     assert len(friends)
-    headers = get_auth_headers(client=client, sample_user_id=2)
+    headers = get_auth_headers(client=client, sample_user_id=5)
     response = client.post(
         f'/users/{user2_id}/friends/{user1["username"]}/delete/',
         headers=headers
@@ -185,8 +193,7 @@ def test_only_friends_can_unfriend():
 
 
 def test_only_invited_user_can_accept():
-    user2 = get_sample_user(2)
-    headers = get_auth_headers(client=client, sample_user_id=2)
+    headers = get_auth_headers(client=client, sample_user_id=5)
     response = client.get(f'/users/{user2_id}/invites/', headers=headers)
     invites = response.json()
     assert len(invites) == 0
@@ -198,26 +205,12 @@ def test_only_invited_user_can_accept():
     assert response.status_code == 422
 
 
-def test_cannot_send_invite_if_there_is_one_sent():
-    user1_id = (client.post('/users/', json=get_sample_user(1))).json()
-    user1 = (client.get(f'/users/{user1_id}')).json()
-    user2 = (client.get(f'/users/{user2_id}')).json()
-    headers = get_auth_headers(client=client, sample_user_id=1)
-    slug = f'/users/{user2_id}/friends/invite/'
-    response = client.post(slug, headers=headers)
-    resent = client.post(slug, headers=headers)
-    
-    assert resent.status_code == 400
-
-
 def test_cannot_send_invite_if_the_users_are_already_friends():
     user1_id = (client.post('/users/', json=get_sample_user(101))).json()
     user2_id = (client.post('/users/', json=get_sample_user(102))).json()
-    user1 = (client.get(f'/users/{user1_id}')).json()
-    user2 = (client.get(f'/users/{user2_id}')).json()
     # send invite
     headers = get_auth_headers(client=client, sample_user_id=101)
-    response = client.post(f'/users/{user2_id}/friends/invite/', headers=headers)
+    response = client.post(f'/users/{user1_id}/friends/invite/{user2_id}/', headers=headers)
     assert response.status_code == 200
     # accept invite
     headers = get_auth_headers(client=client, sample_user_id=102)
@@ -233,19 +226,16 @@ def test_cannot_send_invite_if_the_users_are_already_friends():
     assert response.status_code == 200
     # send another invite
     headers = get_auth_headers(client=client, sample_user_id=101)
-    response = client.post(f'/users/{user2_id}/friends/invite/', headers=headers)
+    response = client.post(f'/users/{user1_id}/friends/invite/{user2_id}/', headers=headers)
     assert response.status_code == 400
 
 
 def test_delete_invite_requires_auth():
     user1_id = (client.post('/users/', json=get_sample_user(121))).json()
     user2_id = (client.post('/users/', json=get_sample_user(122))).json()
-    user1 = (client.get(f'/users/{user1_id}')).json()
-    user2 = (client.get(f'/users/{user2_id}')).json()
     headers = get_auth_headers(client=client, sample_user_id=121)
-    slug = f'/users/{user2_id}/friends/invite/'
-    response = client.post(slug, headers=headers)
-
+    response = client.post(f'/users/{user1_id}/friends/invite/{user2_id}/', headers=headers)
+    assert response.status_code == 200
     headers = get_auth_headers(client=client, sample_user_id=122)
     response = client.get(f'/users/{user2_id}/invites/', headers=headers)
     [invite] = response.json()
@@ -259,11 +249,12 @@ def test_delete_invite_requires_auth():
 def test_delete_invite():
     user1_id = (client.post('/users/', json=get_sample_user(33))).json()
     user2_id = (client.post('/users/', json=get_sample_user(44))).json()
-    user1 = (client.get(f'/users/{user1_id}')).json()
-    user2 = (client.get(f'/users/{user2_id}')).json()
+    user1 = (client.get(f'/users/{user1_id}/')).json()
+    user2 = (client.get(f'/users/{user2_id}/')).json()
     headers = get_auth_headers(client=client, sample_user_id=33)
-    slug = f'/users/{user2_id}/friends/invite/'
+    slug = f'/users/{user1_id}/friends/invite/{user2_id}/'
     response = client.post(slug, headers=headers)
+    assert response.status_code == 200
     friendship = response.json()
     assert friendship['inviting_friend'] == user1['username']
     assert friendship['accepting_friend'] == user2['username']
@@ -287,8 +278,9 @@ def test_cannot_delete_already_accepted_invite():
     user1 = (client.get(f'/users/{user1_id}')).json()
     user2 = (client.get(f'/users/{user2_id}')).json()
     headers = get_auth_headers(client=client, sample_user_id=103)
-    slug = f'/users/{user2_id}/friends/invite/'
+    slug = f'/users/{user1_id}/friends/invite/{user2_id}/'
     response = client.post(slug, headers=headers)
+    assert response.status_code == 200
     friendship = response.json()
     assert friendship['inviting_friend'] == user1['username']
     assert friendship['accepting_friend'] == user2['username']
@@ -313,11 +305,9 @@ def test_cannot_delete_already_accepted_invite():
 def test_user_can_only_accept_own_invite():
     user1_id = (client.post('/users/', json=get_sample_user(105))).json()
     user2_id = (client.post('/users/', json=get_sample_user(106))).json()
-    user1 = (client.get(f'/users/{user1_id}')).json()
-    user2 = (client.get(f'/users/{user2_id}')).json()
     # send invite
     headers = get_auth_headers(client=client, sample_user_id=105)
-    slug = f'/users/{user2_id}/friends/invite/'
+    slug = f'/users/{user1_id}/friends/invite/{user2_id}/'
     response = client.post(slug, headers=headers)
     assert response.status_code == 200
     # accept invite
@@ -339,11 +329,9 @@ def test_user_can_only_accept_own_invite():
 def test_user_can_only_delete_own_invite():
     user1_id = (client.post('/users/', json=get_sample_user(107))).json()
     user2_id = (client.post('/users/', json=get_sample_user(108))).json()
-    user1 = (client.get(f'/users/{user1_id}')).json()
-    user2 = (client.get(f'/users/{user2_id}')).json()
     # send invite
     headers = get_auth_headers(client=client, sample_user_id=107)
-    slug = f'/users/{user2_id}/friends/invite/'
+    slug = f'/users/{user1_id}/friends/invite/{user2_id}/'
     response = client.post(slug, headers=headers)
     assert response.status_code == 200
     # decline invite
